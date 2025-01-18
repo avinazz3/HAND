8ea04523758039c67b5afeefe7157114512bdc62
yuvrajaryan <142132073+yuvrajaryan@users.noreply.github.com>
2025-01-19 00:00:33 +0800

    add broken bet place frontend

---
+++ frontend/src/app/utils/axiosConfig.js
@@ -0,0 +1,13 @@
+// utils/axiosConfig.js
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000'; // Replace with your backend URL
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default axiosInstance;
